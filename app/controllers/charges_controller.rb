4fd17eefaffbf8e1f9b921053efa4a23d5888ca6
Kevin Cruz <kcruz538@gmail.com>
2014-12-08 15:46:51 -0600

    Stripe works

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -7,7 +7,7 @@ class ChargesController < ApplicationController
 
     # create a stripe charge
     Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
-    @charge = Stripe::Charge.create(:amount => 2000,
+    @charge = Stripe::Charge.create(:amount => 10000,
                           :currency => "usd",
                           :card => stripe_token, # obtained with Stripe.js
                           :description => "Charge for #{email}")
